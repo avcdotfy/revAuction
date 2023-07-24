cf90aef60c181493db8b5af2b2c4a55efbc0d373
arun <llpavcdotfy@gmail.com>
2023-07-24 20:08:56 +0530

    added decision making data on UI

--- storage/framework/views/a3cc695e72ea98038a54730665750fdd.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-    Copyright © 2023 <strong> Reverse.Sysaler</strong><strong> | <a href="https://www.dotfy.co" target="_blank">AVC Dotfy
-            LLP</a></strong><br /> All rights reserved.
-</footer>
-<?php /**PATH C:\xampp\htdocs\revAuction\resources\views/vendor/partials/footer.blade.php ENDPATH**/ ?>
