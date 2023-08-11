7ed68dd19df4d6f9d433eff42866916b76eae05b
arun <llpavcdotfy@gmail.com>
2023-08-11 17:23:41 +0530

    added permisison functions , navigation item now can be hiddne

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -13,7 +13,7 @@ class RolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 1; $i < 40; $i++) {
+        for ($i = 1; $i <= 100; $i++) {
             RolePermission::create([
                 'role_id' => 1,
                 'permission_id' => $i,
