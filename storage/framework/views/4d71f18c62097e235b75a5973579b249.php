709e781244a89651858d4edc27f839b74c965f92
arun <llpavcdotfy@gmail.com>
2023-08-16 21:01:07 +0530

    fixed google sheet bugs

--- storage/framework/views/4d71f18c62097e235b75a5973579b249.php
+++ storage/framework/views/4d71f18c62097e235b75a5973579b249.php
@@ -8,22 +8,22 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>
-        {{ env('APP_NAME') }} | Admin | Login
+        <?php echo e(env('APP_NAME')); ?> | Admin | Login
     </title>
     <!-- Favicon -->
-    {{-- <link rel="icon" type="image/png" href="images/logo/favicon.ico" /> --}}
+    
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport" />
     <!-- Bootstrap 3.3.7 -->
-    <link rel="icon" type="image/png" href="{{ asset('media/logo/favDotfy.png') }}" />
+    <link rel="icon" type="image/png" href="<?php echo e(asset('media/logo/favDotfy.png')); ?>" />
 
-    <link rel="stylesheet" href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="<?php echo e(asset('bootstrap/dist/css/bootstrap.min.css')); ?>" />
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}" />
+    <link rel="stylesheet" href="<?php echo e(asset('font-awesome/css/font-awesome.min.css')); ?>" />
     <!-- Ionicons -->
-    <link rel="stylesheet" href="{{ asset('Ionicons/css/ionicons.min.css') }}" />
+    <link rel="stylesheet" href="<?php echo e(asset('Ionicons/css/ionicons.min.css')); ?>" />
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('login/css/AdminLTE.min.css') }}" />
+    <link rel="stylesheet" href="<?php echo e(asset('login/css/AdminLTE.min.css')); ?>" />
     <!-- Google Font -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic" />
@@ -36,7 +36,7 @@
             margin-bottom: 10px;
         }
     </style>
-    <script src="{{ asset('jquery/dist/jquery.min.js') }}"></script>
+    <script src="<?php echo e(asset('jquery/dist/jquery.min.js')); ?>"></script>
     <script type="text/javascript">
         function show_modal() {
             $('#modal_otp').modal('show');
@@ -46,14 +46,15 @@
 
 <body class="hold-transition login-page">
 
-    @yield('login-form')
+    <?php echo $__env->yieldContent('login-form'); ?>
 
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="{{ asset('bootstrap/dist/js/bootstrap.min.js') }}"></script>
-    @stack('scripts')
+    <script src="<?php echo e(asset('bootstrap/dist/js/bootstrap.min.js')); ?>"></script>
+    <?php echo $__env->yieldPushContent('scripts'); ?>
 </body>
 
 
 </html>
+<?php /**PATH C:\xampp\htdocs\revAuctionLatest\resources\views/public/layout/base.blade.php ENDPATH**/ ?>
